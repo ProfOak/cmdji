fb7fd4fa60faae4211dad5ab45f0de067e184c41
ProfOak <OpenProfOak@gmail.com>
2015-01-17 14:36:47 -0600

    added some time stuff, not complete

--- cmdji.go
+++ cmdji.go
@@ -1,9 +1,58 @@
 package main
 
-import "github.com/ProfOak/cmdji/go-bindings"
+import (
+	"fmt"
+	"github.com/ProfOak/cmdji/go-bindings"
+	"io/ioutil"
+	"os/user"
+	"time"
+)
 
 func main() {
-	k := cmdji.Kanji()
-	k.Print()
+	// open file .kanji_today
+	// throw in json.Unmarshal
+	// check date - make date function
+	// if date > .kanji_today date,
 
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	kanji_today_filename := usr.HomeDir + "/.kanji_today"
+
+	// file to store today's kanji
+	// prevents multiple lookups on the server
+	contents, err := ioutil.ReadFile(kanji_today_filename)
+
+	var k cmdji.KanjiADay
+
+	// first time running, or not up to date
+	if err != nil || !upToDate(contents) {
+		// file not found/make file
+		fmt.Println("UPDATING")
+		k = cmdji.Kanji()
+		k.Update()
+		ioutil.WriteFile(kanji_today_filename, k.RawJson(), 0744)
+	} else {
+		// already up to date contents in file
+		k = cmdji.OpenKanji(contents)
+	}
+
+	// ready to use kanjiaday variable
+	k.UnJson()
+	//k.Print()
+
+	// check date to match today (match time zone of server)
+
+	fmt.Println(k.Kanji.Kanji)
+}
+
+func upToDate(contents []byte) bool {
+
+	past_kanji := cmdji.OpenKanji(contents)
+	past_kanji.UnJson()
+	fmt.Println("date", past_kanji.Kanji.Published_at)
+	fmt.Println(time.Now())
+	return true
 }
--- go-bindings/bindings.go
+++ go-bindings/bindings.go
@@ -5,40 +5,24 @@ package cmdji
 */
 import (
 	"encoding/json"
-	//"io/ioutil"
-	//"net/http"
+	"io/ioutil"
+	"net/http"
 	//"strings"
 	"fmt"
 )
 
-func Kanji() kanjiADay {
-	/*
-		// will uncomment this when I impliment the .kanji_today file
-
-		url := "http://one.kanji.website/api/v1/today.json"
-		res, err := http.Get(url)
-		if err != nil {
-		fmt.Println(err)
-		}
-		defer res.Body.Close()
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-		fmt.Println(err)
-		}
-	*/
-	var k kanjiADay
-	var body = []byte(`
-	{"kanji":{"kanji":"示","meanings":["show","indicate","point out","express","display"],"onyomis":["ジ","シ"],"kunyomis":["しめ.す"],"joyo":true,"jlpt":3,"newspaper_rank":237,"max_newspaper_rank":2500,"on_compounds":[["示威運動","ジイウンドウ",["a demonstration"]],["示談","ジダン",["settlement out of court"]],["示威","ジイ",["demonstration","show of force"]],["示指","ジシ",["index finger","forefinger"]],["示寂","ジジャク",["death of a high-ranking priest"]],["示準化石","ジジュンカセキ",["index fossil (fossil which suggests a date of the formation of a rock stratum)","key fossil","leading fossil"]],["示現","ジゲン",["manifestation (of a celestial being)"]],["示達","ジタツ",["instructions","directions"]],["指示","シジ",["indication","instruction","designation","directions"]],["提示","テイジ",["presentation","exhibit","suggest","citation"]],["表示","ヒョウジ",["indication","expression","showing","manifestation","demonstration","display","displaying","representation"]],["掲示","ケイジ",["notice","bulletin","post","posting","placard"]],["示す","シメス",["to (take out and) show","to demonstrate","to tell","to exemplify","to make apparent","to point out (finger","clock hand","needle","etc.)","to indicate","to show","to represent","to signify","to display"]],[" 示唆","シサ",["suggestion","hint","implication"]],["示し","シメシ",["discipline","revelation"]],["示し合わせる","シメシアワセル",["to arrange beforehand","to make a sign to each other","to conspire"]],["示威","ジイ",["demonstration","show of force"]],["示差","シサ",["differential"]],["示相化石","シソウカセキ",["facies fossil"]],["示唆的","シサテキ",["suggestive","pregnant (e.g. pregnant pause)"]],["教示","キョウジ",["instruction","teaching"]],["公示","コウジ",["edict","public announcement"]],["内示","ナイジ",["unofficial announcement"]]],"kun_compounds":[["示す","しめす",["to (take out and) show","to demonstrate","to tell","to exemplify","to make apparent","to point out (finger","clock hand","needle","etc.)","to indicate","to show","to represent","to signify","to display"]],["示偏"," しめすへん",["kanji \"show\" radical at left (radical 113)"]]],"published_at":"2015-01-15T00:00:00.000Z","image":["https://farm4.staticflickr.com/3210/3047742878_944b1cc018_o.jpg","https://www.flickr.com/photos/74743437@N00/3047742878"],"source_url":"http://one.kanji.website/date/2015-01-15"},"home_url":"http://one.kanji.website"}
-	`)
-
-	err := json.Unmarshal(body, &k)
-	if err != nil {
-		fmt.Println("[Fail]", err)
-	}
-
+func Kanji() KanjiADay {
+	var k KanjiADay
 	return k
-
 }
+
+func OpenKanji(b []byte) KanjiADay {
+	var k KanjiADay
+	json_blob = b
+	return k
+}
+
+var json_blob []byte
 
 type kanjiStruct struct {
 	Kanji              string        `json:"kanji"`
@@ -57,12 +41,12 @@ type kanjiStruct struct {
 	Source_url         string        `json:"source_url"`
 }
 
-type kanjiADay struct {
+type KanjiADay struct {
 	Kanji    kanjiStruct `json:"kanji"`
 	Home_url string      `json:"home_url"`
 }
 
-func (k *kanjiADay) Print() {
+func (k *KanjiADay) Print() {
 	// sanity test
 	fmt.Println("Kanji:", k.Kanji.Kanji)
 	fmt.Println("Meanings:", k.Kanji.Meanings)
@@ -79,5 +63,30 @@ func (k *kanjiADay) Print() {
 	fmt.Println("Image:", k.Kanji.Image)
 	fmt.Println("Source_url:", k.Kanji.Source_url)
 	fmt.Println("Home_url:", k.Home_url)
+
+}
+
+func (k *KanjiADay) Update() {
+	const url = "http://one.kanji.website/api/v1/today.json"
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	json_blob, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+func (k *KanjiADay) RawJson() []byte {
+	return json_blob
+}
+func (k *KanjiADay) UnJson() {
+
+	err := json.Unmarshal(json_blob, &k)
+	if err != nil {
+		fmt.Println("[Fail]", err)
+	}
 
 }
